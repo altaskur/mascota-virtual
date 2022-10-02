955301d6f71a5e84d03a2df2cce83b731c55f2bf
Altaskur <105789412+altaskur@users.noreply.github.com>
2022-10-03 01:01:29 +0200

    add animations in css

--- src/js/index.js
+++ src/js/index.js
@@ -20,4 +20,41 @@ function mainLoop() {
   }, 1000);
 }
 
-mainLoop();
+let pet = document.querySelector(".pet");
+let buttons = document.querySelectorAll("input[type=button]");
+buttons[0].addEventListener("click", () => {
+  // get the current class name
+  let currentClass = pet.className;
+
+  console.log(currentClass);
+  if (currentClass.includes("ui-sleep")) {
+    pet.classList.remove("ui-sleep");
+    pet.classList.add("ui-wake-up");
+
+    setTimeout(() => {
+      console.log("wake up");
+      pet.classList.remove("ui-wake-up");
+    }, 1500);
+  } else {
+    pet.classList.add("ui-sleep");
+  }
+});
+
+buttons[1].addEventListener("click", () => {
+  pet.classList.toggle("ui-eat");
+});
+
+buttons[2].addEventListener("click", () => {
+  pet.classList.toggle("ui-greeting");
+});
+
+buttons[3].addEventListener("click", () => {
+  pet.classList.toggle("ui-annoyed");
+  let classList = pet.className;
+  console.log(classList);
+  if (classList.includes("ui-annoyed")) {
+    setTimeout(() => {
+      pet.classList.remove("ui-annoyed");
+    }, 1000);
+  }
+});
